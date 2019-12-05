38ffe21154f390414d20ae47b7d6ec9860a110cc
Youssef Abakhar <yabakhar@e2r5p11.1337.ma>
2019-12-05 22:39:48 +0100

    yabakhar

--- main.c
+++ main.c
@@ -49,12 +49,52 @@ int     output(int str)
   return (0);
 }
 
+char *get_content(t_node *node, int i)
+{	
+	int c = 0;
+	while(node)
+	{
+		if(c == i)
+		{
+			return (node->content);
+		}
+		c++;
+		node =node->next;
+	}
+	return (NULL);
+}
+void	clear_node(t_node *node, int i)
+{
+	while(node)
+	{
+		if(node->index == i)
+		{
+			tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+			if (node->check == 1)
+				tputs(tgetstr("mr", 0), 0, output);
+			ft_putstr_fd(node->content,g_data.fd);
+			tputs(tgetstr("me", 0), 0, output);
+		}
+		node =node->next;
+	}
+}
+
+void	clear_node1(t_node *node, int i)
+{
+	while(node)
+	{
+		if(node->index == i)
+			tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+		node =node->next;
+	}
+}
 void    ft_select(t_node *node, int i)
 {
 	while (node)
 	{
 		if (node->index == i && node->check == 0)
 		{
+			clear_node1(node,i);
 			tputs(tgetstr("mr", 0), 0, output);
 			ft_putstr_fd(node->content,g_data.fd);
 			node->check = 1;
@@ -63,9 +103,11 @@ void    ft_select(t_node *node, int i)
 		}
 		else if (node->index == i && node->check == 1)
 		{
+			clear_node1(node,i);
 			node->check = 0;
 			ft_putstr_fd(node->content,g_data.fd);
 			tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+			tputs(tgetstr("me", 0), 0, output);
 		}
 		node = node->next;
 	}
@@ -84,12 +126,13 @@ int main(int ac , char **av)
 		puts("error");
 	int r = 0;
 	tgetent(buf, getenv("TERM"));
-	tputs(tgetstr("cl", 0), 0, output);
+	tputs(tgetstr("ti", 0), 0, output);
 	int i = 0;
 	int k = 1;
 	int l = 0;
 	t_node *node, *head;
 	head = ft_stock(av);
+	tputs(tgetstr("vi", 0), 0, output);
 	node = head;
 	while (1)
 	{
@@ -107,6 +150,9 @@ int main(int ac , char **av)
 			l = i;
 			i = 0;
 			tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+			tputs(tgetstr("us", 0), 0, output);
+			ft_putstr_fd(node->content,g_data.fd);
+			tputs(tgetstr("me", 0), 0, output);
 		}
 		r = 0;
 		if (read(0, &r, sizeof(int)) > 0)
@@ -115,30 +161,72 @@ int main(int ac , char **av)
 			{
 				if (i > 0)
 				{
+					clear_node(node, i);
 					--i;
-					tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+					if (node->check == 0)
+					{
+						tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+						tputs(tgetstr("us", 0), 0, output);
+						ft_putstr_fd(get_content(node,i),g_data.fd);
+						tputs(tgetstr("ue", 0), 0, output);
+					}
 				}
 				else if (i == 0)
 				{
+					clear_node(node, i);
 					i = l - 1;
-					tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+					if (node->check == 0)
+					{
+						tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+						tputs(tgetstr("us", 0), 0, output);
+						ft_putstr_fd(get_content(node,i),g_data.fd);
+						tputs(tgetstr("ue", 0), 0, output);
+					}
 				}
 			}
 			else if (r == 4348699)
 			{
 				if (i < l - 1)
 				{
+					clear_node(node, i);
 					++i;
-					tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+					if (node->check == 0)
+					{
+						tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+						tputs(tgetstr("us", 0), 0, output);
+						ft_putstr_fd(get_content(node,i),g_data.fd);
+						tputs(tgetstr("ue", 0), 0, output);
+					}
 				}
 				else if (i == l - 1)
 				{
+					clear_node(node, i);
 					i = 0;
-					tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+					if (node->check == 0)
+					{
+						tputs(tgoto(tgetstr("cm", 0), 0, i), 0, output);
+						tputs(tgetstr("us", 0), 0, output);
+						ft_putstr_fd(get_content(node,i),g_data.fd);
+						tputs(tgetstr("ue", 0), 0, output);
+					}
 				}
-			}
-			if (r == 32)
+			} 
+			else if (r == 32)
 				ft_select(node, i);
+			else if (r == 10)
+			{
+				tputs(tgetstr("te", 0), 0, output);
+				while (node)
+				{
+					if (node->check == 1)
+						ft_putendl_fd(node->content, 0);
+					node = node->next;
+				}
+				tputs(tgetstr("ve", 0), 0, output);
+				exit(0);
+			}
 		}
 	}
 }
+
+
